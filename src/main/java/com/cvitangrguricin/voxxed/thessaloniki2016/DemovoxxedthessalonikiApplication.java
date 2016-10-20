301c3b6d733cbc9d71625ce4fef084a3ae30f01e
Cvitan <cvitang@gmail.com>
2016-10-20 10:19:33 +0200

    First commit

---
+++ src/main/java/com/cvitangrguricin/voxxed/thessaloniki2016/DemovoxxedthessalonikiApplication.java
@@ -0,0 +1,12 @@
+package com.cvitangrguricin.voxxed.thessaloniki2016;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemovoxxedthessalonikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemovoxxedthessalonikiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cvitangrguricin/voxxed/thessaloniki2016/HelloControllerTest.java
@@ -0,0 +1,20 @@
+package com.cvitangrguricin.voxxed.thessaloniki2016;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HelloControllerTest {
+	
+	private HelloController controller = new HelloController();
+
+    @Test
+    public void Should_ReturnHello() {
+        // when
+        String result = controller.hello();
+
+        // then
+        assertEquals("hello", result);
+    }
+
+}
